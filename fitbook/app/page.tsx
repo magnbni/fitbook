654f40d697413938a8e009df67bbe74e85054750
endre2112 <endrerm@samfundet.no>
2023-02-13 10:48:05 +0100

    made a navigation bar

--- fitbook/app/page.tsx
+++ fitbook/app/page.tsx
@@ -1,12 +1,12 @@
 import type { NextPage } from "next";
 import Feed from "../components/Feed";
+import Nav from "../components/Nav";
 
 const Home: NextPage = () => {
   return (
     <div className="mt-14 flex justify-center ">
       <div className="w-3/12 flex justify-center fixed left-0">
-        {" "}
-        right panel
+        <Nav />
       </div>
       <div className="w-6/12 flex justify-center">
         <Feed />
--- fitbook/components/Login.tsx
+++ fitbook/components/Login.tsx
@@ -5,14 +5,14 @@ import Image from "next/image";
 
 function Login() {
   return (
-    <div className="flex flex-col align-middle justify-center items-center w-screen h-screen bg-primary text-white">
+    <div className="flex flex-col items-center justify-center w-screen h-screen text-white align-middle bg-primary">
       <p className="text-4xl ">Welcome to</p>
       <p className="text-8xl ">fitbook</p>
       <button
-        className="p-2 bg-white m-10 border-2 flex justify-between items-center px-4"
+        className="flex items-center justify-between p-2 px-4 m-10 bg-white border-2"
         onClick={() => signIn("google")}
       >
-        <p className="font-bold text-primary pr-5">Sign in with google here!</p>
+        <p className="pr-5 font-bold text-primary">Sign in with google here!</p>
         <Image
           src={"/google.png"}
           alt={"google"}
