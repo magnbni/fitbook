78ca0338a4d3aae8029ab5f67115061adbba4921
sofieothilie <sofieod@stud.ntnu.no>
2023-02-15 09:52:44 +0100

    Further improves on profile page

--- fitbook/app/profile/page.tsx
+++ fitbook/app/profile/page.tsx
@@ -1,39 +1,33 @@
-'use client'
+"use client";
 
 import type { NextPage } from "next";
-import { usePathname, useRouter } from "next/navigation";
-import { useSession } from "next-auth/react";
-import { useState, useEffect } from "react";
 import Feed from "../../components/Feed";
+import AddFriend from "../../components/AddFriend";
 
 const Profile: NextPage = () => {
-  // const pathname = usePathname();
-  // const router = useRouter();
-  // const { data: session } = useSession()
-  // const [active, setActive] = useState(false);  
-  // useEffect(() => {
-  //   if (!pathname) return;
-  //   setActive(pathname.includes('/profile'));
-  // }, [pathname]);
-
-  return <div className="w-full flex flex-col">
-      <div className="flex flex-row-02 pl-4 pb-2 space-x-4">
-        <div className="h-full flex justify float-left"> {/* Modify to show users image and username */}
+  return (
+    <div className="w-full flex flex-col top-14 pl-4">
+      <div className="flex flex-row-03 pl-4 pb-4 border-b-4 border-primary border-opacity-50">
+        <div className="h-full w-3/12 flex justify float-left">
+          {/* Modify to show users image and username */}
           <img
-                  className="w-50 h-50 rounded-full shadow-inner"
-                  src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
-                />
+            className="w-50 h-50 rounded-full shadow-inner"
+            src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+          />
         </div>
-        <div className="flex pt-32">
-          <p className="text-primary font-black text-xl">Fred Ole</p>
+        <div className="flex w-9/12 justify-between items-end">
+          <div className="flex flex-col justify-end">
+            <p className="text-primary font-black text-xl">Fred Ole</p>
+            <p className="text-black font text"> @FredOle</p>
+          </div>
+          <AddFriend/>
         </div>
       </div>
-      <div>
-        <Feed/> {/* Customize to only show users posts */}
-      </div>     
-  </div>;
+      <div className="pt-2">
+        <Feed /> {/* Customize to only show users posts */}
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
-
-
