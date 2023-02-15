2458dcd74b9f2feed3b8f688e96d735e92ee0fe3
sofieothilie <sofieod@stud.ntnu.no>
2023-02-15 09:52:04 +0100

    Removes border, minor design fix

--- fitbook/app/page.tsx
+++ fitbook/app/page.tsx
@@ -2,7 +2,10 @@ import type { NextPage } from "next";
 import Feed from "../components/Feed";
 
 const Home: NextPage = () => {
-  return <Feed />;
+  return <>
+  <div className="fixed w-6/12 h-screen shadow-xl top-14 inset-x-1/4"></div>
+  <Feed />;
+  </>
 };
 
 export default Home;
