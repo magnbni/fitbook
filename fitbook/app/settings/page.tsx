5f2a987af90329f01c3b91f2826ee9df9d974ec8
Magnus Brekke Nilsen <magnbni@stud.ntnu.no>
2023-02-21 10:19:59 +0100

    Revert "Merge branch 'feature/pu-18' into 'main'"
    This reverts merge request !17

--- fitbook/app/settings/page.tsx
+++ fitbook/app/settings/page.tsx
@@ -1,49 +1,7 @@
-"use client";
 import type { NextPage } from "next";
-import DeleteUser from "../../components/settings/DeleteUser";
-import ToggleSwitch from "../../components/ToggleSwitch";
 
 const Settings: NextPage = () => {
-  return (
-    <div className="flex items-center flex-col justify-center w-full p-3 bg-green-100 my-2 rounded text-black">
-      <div className="w-full flex flex-row-01">
-        <p className="text-center font-light w-full text-2xl">Settings</p>
-      </div>
-      <div className="flex flex-row-02 p-5 w-full text-left">
-        <p className="font-light">Darkmode:</p>
-        <div className="ml-10">
-          <ToggleSwitch
-            onActivate={() => alert("Activated")}
-            onDisable={() => alert("Disabled")}
-          />
-        </div>
-      </div>
-      <div className="flex flex-row-03 p-5 w-full text-left">
-        <p className="font-light">Show group activity in feed:</p>
-        <div className="ml-10">
-          <ToggleSwitch
-            onActivate={() => alert("Activated")}
-            onDisable={() => alert("Disabled")}
-            activated={true}
-          />
-        </div>
-      </div>
-
-      <div className="flex flex-row-04 p-5 w-full text-left">
-        <p className="font-light">Show friends activity in feed:</p>
-        <div className="ml-10">
-          <ToggleSwitch
-            onActivate={() => alert("Activated")}
-            onDisable={() => alert("Disabled")}
-            activated={true}
-          />
-        </div>
-      </div>
-      <div className="flex flex-row-05 p-5">
-        <DeleteUser />
-      </div>
-    </div>
-  );
+  return <div> Settings</div>;
 };
 
 export default Settings;
