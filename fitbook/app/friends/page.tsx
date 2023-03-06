d740cc790a633ebb4a6e1c4378152ccd265da0d6
Lars <lars.magne.stangeland@gmail.com>
2023-03-06 08:23:59 +0100

    working on connecting friends with db

--- fitbook/app/friends/page.tsx
+++ fitbook/app/friends/page.tsx
@@ -1,54 +1,58 @@
+"use client"
+
 import type { NextPage } from "next";
 import Friend from "../../components/friends/Friend";
 import SearchFriend from "../../components/friends/SearchFriend";
 import { db, firebase } from "../../firebase";
-import { doc, getDoc } from "firebase/firestore";
+import { collection, doc, getDocs } from "firebase/firestore";
 import { getDatabase, ref, onValue, child, get} from "firebase/database"
+import { useState } from "react";
 
 
+type User = {
 
-const fetchfriends = async() => {
-
-
-  const docRef = doc(db, "activeusers", "1");
-  const docSnap = await getDoc(docRef)
-
-  console.log(docSnap.data())
-
-
-
-
+  user_id: string,
 }
 
 
 const Friends: NextPage = () => {
 
-  fetchfriends();
-  let data = [{
 
-    username: "hei",
-    name:"lars",
-    img:"awkd"
+  let users: User[] = [];
 
-  }];
-  return (
-    <div className="w-full">
-      <SearchFriend />
-      <p className="font-light" style={{ marginTop: "30px" }}>
-        Your friends:
-      </p>
-      {data.map((obj) => {
-        return (
-          <Friend
-            key={obj.username}
-            name={obj.name}
-            userName={obj.username}
-            img={obj.img}
-          />
-        );
-      })}
-    </div>
-  );
+  const [users1, setusers] = useState(users);
+
+  const colRef = collection(db, 'users');
+  getDocs(colRef).then((snapshot)=>{
+
+    snapshot.docs.forEach((doc) => {
+
+      users.push({
+        ...doc.data(), user_id: doc.id
+      })
+    })
+
+    setusers(users)
+
+  })
+    return (
+      <div className="w-full">
+        <SearchFriend />
+        <p className="font-light" style={{ marginTop: "30px" }}>
+          Your friends:
+        </p>
+        {users1.map((user) => {
+          return (
+            <Friend
+              key={user.user_id}
+              name={user.user_id}
+              userName={user.user_id}
+              img={user.user_id}
+            />
+          );
+        })}
+      </div>
+    );  
 };
 
 export default Friends;
