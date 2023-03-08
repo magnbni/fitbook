13ad1e133fdb49887346dae6f178475bb64dc2b3
Lars <lars.magne.stangeland@gmail.com>
2023-03-08 08:55:05 +0100

    currently working on fetching data with firebase

--- fitbook/app/friends/page.tsx
+++ fitbook/app/friends/page.tsx
@@ -1,18 +1,12 @@
 "use client"
 
 import type { NextPage } from "next";
-import Friend from "../../components/friends/Friend";
+import Friend, {User} from "../../components/friends/Friend";
 import SearchFriend from "../../components/friends/SearchFriend";
 import { db, firebase } from "../../firebase";
-import { collection, doc, getDocs } from "firebase/firestore";
+import { collection, doc, Firestore, getDocs, onSnapshot} from "firebase/firestore";
 import { getDatabase, ref, onValue, child, get} from "firebase/database"
 import { useState } from "react";
-
-
-type User = {
-
-  user_id: string,
-}
 
 
 const Friends: NextPage = () => {
@@ -20,20 +14,19 @@ const Friends: NextPage = () => {
 
   let users: User[] = [];
 
-  const [users1, setusers] = useState(users);
-
+  //const [users1, setusers] = useState<[]>([]);
   const colRef = collection(db, 'users');
-  getDocs(colRef).then((snapshot)=>{
 
+  onSnapshot(colRef, (snapshot) => {
     snapshot.docs.forEach((doc) => {
-
       users.push({
-        ...doc.data(), user_id: doc.id
+        ...doc.data(),
+        userID: "",
+        name: "",
+        userName: "",
+        img: ""
       })
     })
-
-    setusers(users)
-
   })
     return (
       <div className="w-full">
@@ -41,13 +34,14 @@ const Friends: NextPage = () => {
         <p className="font-light" style={{ marginTop: "30px" }}>
           Your friends:
         </p>
-        {users1.map((user) => {
+        {users.map((user: User) => {
           return (
             <Friend
-              key={user.user_id}
-              name={user.user_id}
-              userName={user.user_id}
-              img={user.user_id}
+              key={user.userID}
+              userID = {user.userID}
+              name={user.name}
+              userName={user.userID}
+              img={user.img}
             />
           );
         })}
--- fitbook/components/friends/Friend.tsx
+++ fitbook/components/friends/Friend.tsx
@@ -1,13 +1,14 @@
 "use client";
 import React from "react";
 
-type Props = {
+export type User = {
+  userID: string
   name: string;
   userName: string;
   img: string;
 };
 
-const Friend = ({ name, userName, img }: Props) => {
+const Friend = ({ name, userName, img }: User) => {
   const onClick = () => {
     alert(`@${userName} is no longer your friend.`);
   };
