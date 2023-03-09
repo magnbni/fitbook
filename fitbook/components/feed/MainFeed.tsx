2e9e0d3f40209dbacd0629a1c6daf15d67274007
sofieothilie <sofieod@stud.ntnu.no>
2023-03-09 13:56:47 +0100

    Improves mainfeed

--- fitbook/components/feed/MainFeed.tsx
+++ fitbook/components/feed/MainFeed.tsx
@@ -12,11 +12,9 @@ import { signOut, useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { db } from "../../firebase";
 import FitbookPostWithImage from "./FitbookPostWithImage";
+import Post from "./Post";
 
-function UserFeed() {
-  const [friends, setFriends] = useState<
-    { username: string; profilepic: string }[]
-  >([]);
+function MainFeed() {
   const [posts, setPosts] = useState<
     {
       id: string;
@@ -31,9 +29,19 @@ function UserFeed() {
     findPosts();
   }, []);
 
-  const findPosts = async () => {
-    findFriends();
-    for (const { username, profilepic } of friends) {
+  async function findPosts() {
+    const friends = await findFriends();
+    const newPosts: {
+      id: string;
+      username: string;
+      profilepic: string;
+      postText: string;
+      postPicture: string;
+      timestamp: Timestamp;
+    }[] = [];
+    for (const { username } of friends) {
+      const docRef = doc(db, "users", username);
+      const docSnap = await getDoc(docRef);
       const postRef = collection(db, "users", username, "posts");
       const postQuery = query(postRef);
 
@@ -44,20 +52,20 @@ function UserFeed() {
         return {
           id: doc.id,
           username: username,
-          profilepic: profilepic,
+          profilepic: docSnap.get("picture"),
           postText: data.postText,
           postPicture: data.postPicture,
           timestamp: data.timestamp,
         };
       });
-      posts.push(...posts);
+      newPosts.push(...posts);
     }
-    setPosts(posts);
-    posts.sort((a, b) => b.timestamp.seconds - a.timestamp.seconds);
-  };
+    newPosts.sort((a, b) => b.timestamp.seconds - a.timestamp.seconds);
+    setPosts(newPosts);
+  }
 
   // Get a reference to the subcollection
-  const findFriends = async () => {
+  async function findFriends() {
     const username = await findUser();
 
     const friendRef = collection(db, "users", username, "friends");
@@ -71,11 +79,10 @@ function UserFeed() {
       const data = doc.data();
       return {
         username: data.username,
-        profilepic: data.picture,
       };
     });
-    setFriends(friends);
-  };
+    return friends;
+  }
 
   const { data: session } = useSession();
   const findUser = async () => {
@@ -88,7 +95,8 @@ function UserFeed() {
       } else {
         return docSnap.get("username");
       }
-    } else { //DETTE ER IKKE BRA <3
+    } else {
+      //DETTE ER IKKE BRA <3
       const docRef = doc(db, "activeUsers", "1");
       const docSnap = await getDoc(docRef);
       if (!docSnap.exists()) {
@@ -102,22 +110,25 @@ function UserFeed() {
 
   return (
     <div className="flex flex-col w-full pl-4 top-14">
-      {posts.map((post) => {
-        return (
-          <div className="p-4 my-2 border-2 rounded " key={post.id}>
-            {post.postPicture && post.postPicture.length > 0 && (
-              <FitbookPostWithImage
-                text={post.postText}
-                username={post.username}
-                imageUrl={post.postPicture}
-                profilepic={post.profilepic}
-              />
-            )}
-          </div>
-        );
-      })}
+      <Post />
+      <div className="flex flex-col w-full pl-4 top-14">
+        {posts.map((post) => {
+          return (
+            <div className="p-4 my-2 border-2 rounded " key={post.id}>
+              {post.postPicture && post.postPicture.length > 0 && (
+                <FitbookPostWithImage
+                  text={post.postText}
+                  username={post.username}
+                  imageUrl={post.postPicture}
+                  profilepic={post.profilepic}
+                />
+              )}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
 
-export default UserFeed;
+export default MainFeed;
